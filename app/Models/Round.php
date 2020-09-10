ee8145e50e186b1885dd8f5770ab5ccc8e28e3c5
Fran Leston <franciscoleston@mintyhost.es>
2020-09-10 16:19:00 +0200

    api fixes

---
+++ app/Models/Round.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Round extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'league_id', 'league_name', 'round', 'is_current',
+    ];
+
+    /**
+     * Get the fixture that owns the round
+     */
+    public function fixture()
+    {
+        return $this->belongsTo('App\Models\Fixture');
+    }
+}
---
+++ database/migrations/2020_09_10_094944_create_rounds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoundsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('league_id')->constrained();
+            $table->string('round')->nullable();
+            $table->boolean('is_current')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rounds');
+    }
+}
