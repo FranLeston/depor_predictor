d0324df99fd207b0cd3c53cfe928b5398f07390d
Fran Leston <lestonramos@gmail.com>
2020-09-20 20:41:32 +0200

    sql

--- deploy.php
+++ deploy.php
@@ -12,9 +12,6 @@ set('repository', 'https://github.com/FranLeston/depor_predictor');
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 
-host('root@157.230.107.10')
-    ->set('deploy_path', '/var/www/html');
-
 // Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', []);
@@ -24,7 +21,7 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('fran@165.22.66.154')
+host('root@165.22.66.154')
     ->set('deploy_path', '/var/www/depor_predictor')
     ->set('branch', 'master');
 
@@ -32,20 +29,6 @@ host('fran@165.22.66.154')
 
 task('build', function () {
     run('cd {{release_path}} && build');
-});
-
-task('deploy:config', function () {
-    // For arguments
-    $stage = null;
-    if (input()->hasArgument('stage')) {
-        $stage = input()->getArgument('stage');
-        write('Remove shared environment file');
-        run('rm {{deploy_path}}/shared/.env');
-        write('Copying {{release_path}}/.env.' . $stage . ' as config file...');
-        run('cp {{release_path}}/.env.' . $stage . ' {{deploy_path}}/shared/.env');
-    } else {
-        write('No config file copied!');
-    }
 });
 
 // [Optional] if deploy fails automatically unlock.
